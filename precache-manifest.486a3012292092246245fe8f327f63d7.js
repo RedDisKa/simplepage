4804bd89f82fbe5e39f93379f5eada5683790aa2
AnastasiaMineva <anastasia.mineva@gmail.com>
2020-10-13 12:55:54 +0800

    Updates

---
+++ precache-manifest.486a3012292092246245fe8f327f63d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606c1c91a86d6faa4fa851d8ebe2d6a5",
+    "url": "/simplepage/index.html"
+  },
+  {
+    "revision": "87e43166089ee5d9c884",
+    "url": "/simplepage/static/css/main.423b20bb.chunk.css"
+  },
+  {
+    "revision": "ed9fc9e984b16bcae84d",
+    "url": "/simplepage/static/js/2.dbfc0308.chunk.js"
+  },
+  {
+    "revision": "fae377d30f2a087ba5c4f2f1d9c0d180",
+    "url": "/simplepage/static/js/2.dbfc0308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87e43166089ee5d9c884",
+    "url": "/simplepage/static/js/main.c1b19b43.chunk.js"
+  },
+  {
+    "revision": "0d700cfac28b97c77115",
+    "url": "/simplepage/static/js/runtime-main.d28ef835.js"
+  },
+  {
+    "revision": "f61a4eb27371b7453bf5b12ab3648b9e",
+    "url": "/simplepage/static/media/Poppins-Medium.f61a4eb2.ttf"
+  },
+  {
+    "revision": "4cdacb8f89d588d69e8570edcbe49507",
+    "url": "/simplepage/static/media/Poppins-SemiBold.4cdacb8f.ttf"
+  }
+]);
